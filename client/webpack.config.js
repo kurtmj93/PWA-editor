edf8a66d77f3b8d6e796746d1f73a1b37fbdfa2a
Kurtmj93 <kurtmj93@gmail.com>
2023-04-11 15:01:55 -0400

    add and configure workbox plugins, manifest file, add babel loader to webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -17,16 +14,45 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
-    plugins: [
-      
+    plugins: [ // TODO: Add and configure workbox plugins for a service worker and manifest file.
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      title: 'Just Another Text Editor',
+    }),
+    new WebpackPwaManifest({
+      name: 'Just Another Text Editor',
+      short_name: 'JATE',
+      description: 'What you see is what you get.',
+      background_color: '#ffffff',
+      icons: [
+        {
+          src: path.resolve('src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
+        },
+      ]
+    }),
+    new InjectManifest({
+      swSrc: './src-sw.js',
+      swDest: 'src-sw.js'
+    })
     ],
 
     module: {
       rules: [
-        {
+        { // Add CSS loaders to webpack.
           test: /\.css$/i,
           use: ['style-loader', 'css-loader'],
         },
+        { // Add Babel to webpack
+          test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+          }
+        }
       ],
     },
   };
