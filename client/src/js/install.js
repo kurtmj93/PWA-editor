66b979cbad100bac2e70752e096b66e45a9bccaf
Kurtmj93 <kurtmj93@gmail.com>
2023-04-12 11:57:26 -0400

    add handlers to install.js for prompting install - issue: button not hidden after install

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,35 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+// event handler for `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+    // store triggered events
+    window.deferredPrompt = event;
+    // remove hidden class from button
+    butInstall.classList.toggle('hidden', false);
+});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+// click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+
+    if (!promptEvent) {
+        return;
+    }
+
+    // show prompt
+    promptEvent.prompt();
+
+    window.deferredPrompt = null; // reset deferred prompt variable
+    // prevents prompt from installing more than once
+
+    butInstall.classList.toggle('hidden', true);
+    // hide install button after install
+});
+
+// handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+    window.deferredPrompt = null; 
+    // prevents prompt from installing more than once
+});
